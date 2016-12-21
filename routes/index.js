b3d1c2497d01d348bb17a43ca746200cde578aa5
Erik Eriksson Liu <Erik Eriksson Liu>
2016-12-21 19:51:01 +0100

    wiringpi added

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,16 @@ var path = require('path');
 
 var request = require("request");
 
+var wpi = require('wiring-pi');
+wpi.setup('wpi');
+
+var pin = 7;
+
+wpi.pinMode(pin, wpi.OUTPUT);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Remote', deskIsOn : 'Lampa' });
+	res.render('index', { title: 'Home Remote', deskIsOn : 'Lampa' });
    // res.render('start', { title: 'Home Remote', deskIsOn : 'Lampa' });
     // res.sendFile(path.join(__dirname+'/start.html'));
   // res.send("test");
@@ -17,11 +24,12 @@ router.post('/bed-red', function(req, res){
 	var val = req.body.val;
 	console.log("Value " + val);
 	request.get({url: "http://192.168.1.50", 
-             qs: {red:val}},
-            function(error, response, body){
-               console.log(res.statusCode);
-               console.log(res.headers);
-            });
+		qs: {red:val}},
+		function(error, response, body){
+			console.log(res.statusCode);
+			console.log(res.headers);
+		});
+	res.end();
 });
 
 router.post('/bed-blue', function(req, res){
@@ -29,13 +37,25 @@ router.post('/bed-blue', function(req, res){
 	var val = req.body.val;
 	console.log("Value " + val);
 	request.get({url: "http://192.168.1.50", 
-             qs: {blu:val}},
-            function(error, response, body){
-               console.log(res.statusCode);
-               console.log(res.headers);
-            });
+		qs: {blu:val},
+		agent : false
+	},
+	function(error, response, body){
+		console.log(res.statusCode);
+		console.log(res.headers);
+	});
+	res.end();
 });
 
+router.post('/desk', function(req, res){
+	var val = req.body.val;
+	if(val == 'on'){
+		wpi.digitalWrite(pin, 1);
+	}else{
+		wpi.digitalWrite(pin, 0);
+	}
+	req.end();
+});
 
 
 
